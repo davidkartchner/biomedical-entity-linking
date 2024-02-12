2612ca53b27f041445e9128da499a0797de0f5cd
Bathala_Prasanth_pbathala3 <pbathala3@MITCH-LIN.AD.GATECH.EDU>
2024-02-11 20:33:28 -0500

    Updated Ontology.py

--- ontology.py
+++ ontology.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from collections import defaultdict
 from typing import List, Optional, Union
+from tqdm.auto import tqdm
 
 @dataclass
 class BiomedicalEntity:
@@ -23,17 +25,48 @@ class BiomedicalOntology:
     entities: dict # Dict mapping CUI: BiomedicalEntity
     # typed_entities: dict
 
+    def load_data(self, path):
+        with open(path, "r") as f:
+            self.raw_data = f.readlines()
+
+    def preprocess(self):
+        """
+        Extract only English Names, Process to extract the cui, synonyms.
+        """
+        processed = []
+        for l in tqdm(self.raw_data):
+            l_data = l.rstrip('\n').split("|")
+            cui, lang, synonym = l_data[0], l_data[1], l_data[14]
+            if lang != 'ENG':
+                continue
+            row = cui + "||" + synonym.lower()
+            processed.append(row)
+        
+        self.alias_to_cui = defaultdict(list)
+        processed = list(set(processed)) # Remove duplicates
+        for line in tqdm(processed):
+            cui, name = line.split('||')
+            if cui in self.entities:
+                self.entities.append(name)
+            else:
+                self.entities[cui] = [name]
+        
+
+            self.alias_to_cui[name].append(cui)
+
+
+            
     def get_aliases(self, cui=None):
         '''
         Get aliases for a particular CUI.  If cui=None, provide a mapping of {cui: [aliases]}
         '''
-        pass
+        return self.entities[cui]
 
     def get_entities_with_alias(self, alias=None):
         '''
         Get all entities sharing a particular alias.  If alias=None, return a mapping of {alias: [cuis]}
         '''
-        pass
+        return self.alias_to_cui[alias]
 
     def get_definitions(self, cui):
         pass
@@ -60,3 +93,8 @@ class BiomedicalOntology:
 @dataclass
 class CompositeOntology:
     ontologies: dict # Dict of each ontology used
+
+
+ontology = BiomedicalOntology()
+ontology.load_data("/mitchell/entity-linking/2022AA/mitchell/entity-linking/2022AA/META/MRCONSO.RRF")
+ontology.preprocess()
