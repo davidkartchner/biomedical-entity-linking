ef662310531ed8f899122deb9b897a812f57ac82
David_Kartchner <dkartchner3@gatech.edu>
2024-02-13 14:26:37 -0500

    moving ontology into package

--- package/ontology.py
+++ package/ontology.py
@@ -1,13 +1,11 @@
 from dataclasses import dataclass
 from typing import List, Optional, Union
 
-
 @dataclass
 class BiomedicalEntity:
     """
     Class for keeping track of all relevant fields in an ontology
     """
-
     cui: str
     name: str
     types: List[str]
@@ -15,12 +13,50 @@ class BiomedicalEntity:
     definition: Optional[str]
     equivalant_cuis: Optional[List[str]]
     taxonomy: Optional[str]
-
+    extra_data: Optional(dict)
 
 @dataclass
 class BiomedicalOntology:
-    entities: List[BiomedicalEntity]
+    name: str
+    types: List[str]
+    abbrev: Optional[str] # Abbreviated name of ontology if different than name
+    entities: dict # Dict mapping CUI: BiomedicalEntity
     # typed_entities: dict
 
-    def from_obo(self, entity_type=None):
+    def get_aliases(self, cui=None):
+        '''
+        Get aliases for a particular CUI.  If cui=None, provide a mapping of {cui: [aliases]}
+        '''
         pass
+
+    def get_entities_with_alias(self, alias=None):
+        '''
+        Get all entities sharing a particular alias.  If alias=None, return a mapping of {alias: [cuis]}
+        '''
+        pass
+
+    def get_definitions(self, cui):
+        pass
+
+    def from_obo(self, filepath=None):
+        pass
+
+    def from_umls(self):
+        pass
+
+    def from_mesh(self):
+        pass
+
+    def from_ncbi_taxon(self):
+        pass
+
+    def from_csv(self):
+        pass
+
+    def from_json(self):
+        pass
+
+
+@dataclass
+class CompositeOntology:
+    ontologies: dict # Dict of each ontology used
