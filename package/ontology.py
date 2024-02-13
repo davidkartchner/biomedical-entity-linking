80265dd1160243b031aaad2b20785bf572d1d043
David_Kartchner <dkartchner3@gatech.edu>
2024-02-13 17:48:05 -0500

    first pass at obo loader for ontology

--- package/ontology.py
+++ package/ontology.py
@@ -1,5 +1,15 @@
 from dataclasses import dataclass
 from typing import List, Optional, Union
+from tqdm import tqdm
+
+import obonet
+
+from .utils import _obo_extract_definition, _obo_extract_synonyms
+from .logger import setup_logger
+
+logger = setup_logger()
+
+
 
 @dataclass
 class BiomedicalEntity:
@@ -13,14 +23,14 @@ class BiomedicalEntity:
     definition: Optional[str]
     equivalant_cuis: Optional[List[str]]
     taxonomy: Optional[str]
-    extra_data: Optional(dict)
+    extra_data: Optional[dict]
 
 @dataclass
 class BiomedicalOntology:
     name: str
     types: List[str]
+    entities: List[BiomedicalEntity] # Dict mapping CUI: BiomedicalEntity
     abbrev: Optional[str] # Abbreviated name of ontology if different than name
-    entities: dict # Dict mapping CUI: BiomedicalEntity
     # typed_entities: dict
 
     def get_aliases(self, cui=None):
@@ -38,25 +48,90 @@ class BiomedicalOntology:
     def get_definitions(self, cui):
         pass
 
-    def from_obo(self, filepath=None):
+    def load_obo(self, filepath, name=None, prefix_to_keep=None, entity_type=None, abbrev=None):
+        '''
+        Read an ontology in .obo format
+
+        Parameters:
+        ----------------------
+            filepath: str (optional)
+                Path to .obo formatted ontology.  Can be a URL.  
+            prefix_to_keep: str (optional)
+        '''
+
+        entities = []
+        if entity_type:
+            types = [entity_type]
+        else:
+            types = []
+        ontology = obonet.read_obo(filepath)
+
+        for curie, data in tqdm(ontology.nodes(data=True)):
+            # Exclude CUIs that are from cross-referenced ontologies
+            if prefix_to_keep is not None:
+                if not curie.startswith(prefix_to_keep):
+                    continue
+
+            # Extract name and synonyms
+            if 'name' not in data:
+                synonyms = _obo_extract_synonyms(data)
+            else:
+                synonyms = [data['name']] + _obo_extract_synonyms(data)
+            
+            # Skip entities with no name/aliases
+            if len(synonyms) == 0:
+                logger.warning(f"Data entry for CUI {curie} has no listed names/aliases.  Skipping.")
+                continue
+            name = synonyms[0]
+            if len(synonyms) > 1:
+                other_synonyms = synonyms[1:]
+            else:
+                other_synonyms = []
+
+            # Get definition if it exists
+            definition = _obo_extract_definition(data)
+
+            
+            ent = BiomedicalEntity(cui=curie, name=name, aliases=other_synonyms, types=types, definition=definition)
+            entities.append(ent)
+
+        self.entities = entities
+        self.types = types
+        if name:
+            self.name = name
+        elif filepath.startswith('http'):
+            parsed_name = filepath.split('/')[-1].split('.')[0]
+            logger.warning(f"No ontology name provided.  Using name from provided URL: {parsed_name}")
+            self.name = parsed_name
+        if abbrev:
+            self.abbrev = abbrev
+
+        
+            
+        # synonyms = _obo_term_to_synonyms(ontology, filter_prefix=prefix_to_keep)
+        # definitions = _obo_term_to_definitions(ontology, filter_prefix=prefix_to_keep)
+        # for key 
         pass
 
-    def from_umls(self):
+    def load_umls(self, umls_dir):
         pass
 
-    def from_mesh(self):
+    def load_mesh(self, mesh_dir):
         pass
 
-    def from_ncbi_taxon(self):
+    def load_ncbi_taxon(self):
         pass
 
-    def from_csv(self):
+    def load_csv(self):
         pass
 
-    def from_json(self):
+    def load_json(self):
         pass
 
 
 @dataclass
 class CompositeOntology:
     ontologies: dict # Dict of each ontology used
+
+    def load_from_config(self, config=None, config_path=None):
+        pass
