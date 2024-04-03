8f7a80b348ad03671d5e49a374631e3388336749
Ye_Christophe_cye73 <cye73@MITCH-LIN.AD.GATECH.EDU>
2024-04-03 11:34:52 -0400

    Add load_mesh in ontology.py

--- bioel/ontology.py
+++ bioel/ontology.py
@@ -6,9 +6,9 @@ import obonet
 
 from .utils import _obo_extract_definition, _obo_extract_synonyms
 from .logger import setup_logger
+from umls_utils import UmlsMappings
 
 logger = setup_logger()
-
 
 
 @dataclass
@@ -16,6 +16,7 @@ class BiomedicalEntity:
     """
     Class for keeping track of all relevant fields in an ontology
     """
+
     cui: str
     name: str
     types: List[str]
@@ -25,66 +26,75 @@ class BiomedicalEntity:
     taxonomy: Optional[str] = None
     metadata: Optional[dict] = None
 
+
 @dataclass
 class BiomedicalOntology:
     name: str
     types: List[str] = field(default_factory=list)
-    entities: List[BiomedicalEntity] = field(default_factory=list) # Dict mapping CUI: BiomedicalEntity
-    abbrev: Optional[str] = None # Abbreviated name of ontology if different than name
+    entities: List[BiomedicalEntity] = field(
+        default_factory=list
+    )  # Dict mapping CUI: BiomedicalEntity
+    abbrev: Optional[str] = None  # Abbreviated name of ontology if different than name
     metadata: Optional[dict] = None
 
     def get_canonical_name(self):
-        '''
+        """
         Get name of entities in the ontology
         data: list of dict
-        '''
+        """
         canonical_names = {entity.cui: entity.name for entity in self.entities}
         return canonical_names
-    
+
     def get_aliases(self):
-        '''
+        """
         Get aliases of entities in the ontology
         data: list of dict
-        '''
+        """
         aliases = {entity.cui: entity.aliases for entity in self.entities}
         return aliases
-    
+
     def get_definition(self):
-        '''
+        """
         Get definition of entities in the ontology
         data: list of dict
-        '''
-        definitions_dict = {entity.cui: entity.definition for entity in self.entities if entity.definition is not None}
+        """
+        definitions_dict = {
+            entity.cui: entity.definition
+            for entity in self.entities
+            if entity.definition is not None
+        }
         return definitions_dict
-    
+
     def get_types(self):
-        '''
+        """
         Get type of entities in the ontology
         data: list of dict
-        '''
+        """
         # Extract tuples of CUI and types from the Data
         types = {entity.cui: entity.types for entity in self.entities}
         return types
 
     @classmethod
-    def load_obo(cls, filepath, name=None, prefix_to_keep=None, entity_type=None, abbrev=None):
-        '''
+    def load_obo(
+        cls, filepath, name=None, prefix_to_keep=None, entity_type=None, abbrev=None
+    ):
+        """
         Read an ontology in .obo format
 
         Parameters:
         ----------------------
             filepath: str (optional)
-                Path to .obo formatted ontology.  Can be a URL.  
+                Path to .obo formatted ontology.  Can be a URL.
             prefix_to_keep: str (optional)
-        '''
+        """
 
         entities = []
         if entity_type:
             types = [entity_type]
         else:
             types = []
-        
-        logger.info(f'Reading OBO ontology from {filepath}')
+
+        logger.info(f"Reading OBO ontology from {filepath}")
         ontology = obonet.read_obo(filepath)
 
         data_keys = set([])
@@ -92,28 +102,31 @@ class BiomedicalOntology:
             # Exclude CUIs that are from cross-referenced ontologies
             data_keys.update(set(data.keys()))
 
-
             if prefix_to_keep is not None:
                 if not curie.startswith(prefix_to_keep):
                     continue
 
             # Extract name and synonyms
-            if 'name' not in data:
+            if "name" not in data:
                 synonyms = _obo_extract_synonyms(data)
             else:
-                synonyms = [data['name']] + _obo_extract_synonyms(data)
+                synonyms = [data["name"]] + _obo_extract_synonyms(data)
 
             # Include deprecated CUIs as alternative ids
             alt_cuis = None
-            if 'alt_id' in data and len(data['alt_id']) > 0:
+            if "alt_id" in data and len(data["alt_id"]) > 0:
                 if prefix_to_keep:
-                    alt_cuis = [x for x in data['alt_id'] if x.startswith(prefix_to_keep)]
+                    alt_cuis = [
+                        x for x in data["alt_id"] if x.startswith(prefix_to_keep)
+                    ]
                 else:
-                    alt_cuis = data['alt_id']
-            
+                    alt_cuis = data["alt_id"]
+
             # Skip entities with no name/aliases
             if len(synonyms) == 0:
-                logger.warning(f"Data entry for CUI {curie} has no listed names/aliases.  Skipping.")
+                logger.warning(
+                    f"Data entry for CUI {curie} has no listed names/aliases.  Skipping."
+                )
                 continue
             ent_name = synonyms[0]
             if len(synonyms) > 1:
@@ -124,30 +137,88 @@ class BiomedicalOntology:
             # Get definition if it exists
             definition = _obo_extract_definition(data)
 
-            
-            ent = BiomedicalEntity(cui=curie, name=ent_name, aliases=other_synonyms, types=types, definition=definition, equivalant_cuis=alt_cuis)
+            ent = BiomedicalEntity(
+                cui=curie,
+                name=ent_name,
+                aliases=other_synonyms,
+                types=types,
+                definition=definition,
+                equivalant_cuis=alt_cuis,
+            )
             entities.append(ent)
 
-        if not name :
-            if filepath.startswith('http'):
-                parsed_name = filepath.split('/')[-1].split('.')[0]
-                logger.warning(f"No ontology name provided.  Using name from provided URL: {parsed_name}")
+        if not name:
+            if filepath.startswith("http"):
+                parsed_name = filepath.split("/")[-1].split(".")[0]
+                logger.warning(
+                    f"No ontology name provided.  Using name from provided URL: {parsed_name}"
+                )
                 name = parsed_name
 
         return cls(entities=entities, types=types, name=name, abbrev=abbrev)
 
-        
-            
         # synonyms = _obo_term_to_synonyms(ontology, filter_prefix=prefix_to_keep)
         # definitions = _obo_term_to_definitions(ontology, filter_prefix=prefix_to_keep)
-        # for key 
+        # for key
         pass
 
     def load_umls(self, umls_dir):
         pass
 
-    def load_mesh(self, mesh_dir):
-        pass
+    def load_mesh(self, path=None, api_key=""):
+        umls = UmlsMappings(umls_dir=path, umls_api_key=api_key)
+
+        # Get the Canonial Names
+        lowercase = False
+        mesh_to_name = umls.get_canonical_name(
+            ontologies_to_include=["MSH"],
+            use_umls_curies=False,
+            mapping_cols={"MSH": "sdui"},
+            prefixes={"MSH": "MESH"},
+            lowercase=lowercase,
+        )
+
+        mesh_to_alias = umls.get_aliases(
+            ontologies_to_include=["MSH"],
+            use_umls_curies=False,
+            mapping_cols={"MSH": "sdui"},
+            prefixes={"MSH": "MESH"},
+            lowercase=lowercase,
+        )
+
+        mesh_cui2definition = umls.get_definition(
+            ontologies_to_include=["MSH"],
+            use_umls_curies=False,
+            mapping_cols={"MSH": "sdui"},
+            prefixes={"MSH": "MESH"},
+            lowercase=lowercase,
+        )
+
+        mesh_to_types, mesh_to_groups = umls.get_types_and_groups(
+            ontologies_to_include=["MSH"],
+            use_umls_curies=False,
+            mapping_cols={"MSH": "sdui"},
+            prefixes={"MSH": "MESH"},
+            lowercase=lowercase,
+        )
+
+        for cui, name in tqdm(mesh_to_name.items()):
+            ent_type = mesh_to_types[cui]
+            other_aliases = [x for x in mesh_to_alias[cui] if x != name]
+            joined_aliases = " ; ".join(other_aliases)
+            entity = BiomedicalEntity(
+                cui=f"MESH:{cui}",
+                name=name,
+                types=ent_type,
+                aliases=joined_aliases,
+                definition=(
+                    mesh_cui2definition[cui] if cui in mesh_cui2definition else None
+                ),
+                metadata={
+                    "group": mesh_to_groups[cui],
+                },
+            )
+            self.entities.append(entity)
 
     def load_ncbi_taxon(self):
         pass
@@ -161,7 +232,7 @@ class BiomedicalOntology:
 
 @dataclass
 class CompositeOntology:
-    ontologies: dict # Dict of each ontology used
+    ontologies: dict  # Dict of each ontology used
 
     def load_from_config(self, config=None, config_path=None):
         pass
