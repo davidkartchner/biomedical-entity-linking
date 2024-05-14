70baf3d125a0fc70168b61a73dbd427b8f8da096
Ye_Christophe_cye73 <cye73@MITCH-LIN.AD.GATECH.EDU>
2024-05-14 16:46:38 -0400

    Adding tests

--- bioel/tests/test_model.py
+++ bioel/tests/test_model.py
@@ -25,150 +25,150 @@ import ujson
 
 class TestModel(unittest.TestCase):
 
-    # def test_train_arboel_biencoder(self):
-    #     """
-    #     TestCase - 1: Test the training part of Biencoder
-    #     """
+    def test_train_arboel_biencoder(self):
+        """
+        TestCase - 1: Test the training part of Biencoder
+        """
 
-    #     # Load configuration file
-    #     with open("config_arboel.json", "r") as f:
-    #         config = ujson.load(f)
+        # Load configuration file
+        with open("config_arboel.json", "r") as f:
+            config = ujson.load(f)
 
-    #     test_cases = config["test_cases_train_biencoder"]
+        test_cases = config["test_cases_train_biencoder"]
 
-    #     parser = BlinkParser(add_model_args=True)
-    #     parser.add_training_args()
+        parser = BlinkParser(add_model_args=True)
+        parser.add_training_args()
 
-    #     for case in test_cases:
-    #         test_args = [
-    #             "--data_path",
-    #             case["data_path"],
-    #             "--output_path",
-    #             case["output_path"],
-    #             "--dataset",
-    #             case["dataset"],
-    #             "--ontology",
-    #             case["ontology"],
-    #             "--load_function",
-    #             case["load_function"],
-    #             "--model_name_or_path",
-    #             "dmis-lab/biobert-base-cased-v1.2",
-    #         ]
+        for case in test_cases:
+            test_args = [
+                "--data_path",
+                case["data_path"],
+                "--output_path",
+                case["output_path"],
+                "--dataset",
+                case["dataset"],
+                "--ontology",
+                case["ontology"],
+                "--load_function",
+                case["load_function"],
+                "--model_name_or_path",
+                "dmis-lab/biobert-base-cased-v1.2",
+            ]
 
-    #         args = parser.parse_args(test_args)
-    #         args_dict = args.__dict__
-    #         args_dict["ontology_dict"] = case["ontology_dict"]
+            args = parser.parse_args(test_args)
+            args_dict = args.__dict__
+            args_dict["ontology_dict"] = case["ontology_dict"]
 
-    #         # Check if the function name provided is an actual function of BiomedicalOntology
-    #         if hasattr(BiomedicalOntology, args_dict["load_function"]):
-    #             load_func = getattr(BiomedicalOntology, args_dict["load_function"])
+            # Check if the function name provided is an actual function of BiomedicalOntology
+            if hasattr(BiomedicalOntology, args_dict["load_function"]):
+                load_func = getattr(BiomedicalOntology, args_dict["load_function"])
 
-    #             if args_dict["ontology_dict"]:
-    #                 ontology_object = load_func(**args_dict["ontology_dict"])
-    #                 print(f"Ontology loaded successfully. Name: {ontology_object.name}")
-    #             else:
-    #                 raise ValueError("No ontology data provided.")
-    #         else:
-    #             raise ValueError(
-    #                 f"Error: {args_dict['load_function']} is not a valid function for BiomedicalOntology."
-    #             )
+                if args_dict["ontology_dict"]:
+                    ontology_object = load_func(**args_dict["ontology_dict"])
+                    print(f"Ontology loaded successfully. Name: {ontology_object.name}")
+                else:
+                    raise ValueError("No ontology data provided.")
+            else:
+                raise ValueError(
+                    f"Error: {args_dict['load_function']} is not a valid function for BiomedicalOntology."
+                )
 
-    #         with open(
-    #             os.path.join(
-    #                 args_dict["data_path"], f"{args_dict['ontology']}_object.pickle"
-    #             ),
-    #             "wb",
-    #         ) as f:
-    #             pickle.dump(ontology_object, f)
+            with open(
+                os.path.join(
+                    args_dict["data_path"], f"{args_dict['ontology']}_object.pickle"
+                ),
+                "wb",
+            ) as f:
+                pickle.dump(ontology_object, f)
 
-    #         data_module = ArboelDataModule(params=args_dict)
+            data_module = ArboelDataModule(params=args_dict)
 
-    #         model = LitArboel(params=args_dict)
+            model = LitArboel(params=args_dict)
 
-    #         trainer = L.Trainer(
-    #             num_sanity_val_steps=0,
-    #             limit_train_batches=1,
-    #             limit_val_batches=1,
-    #             max_epochs=1,
-    #             devices=[0],
-    #             accelerator="gpu",
-    #             strategy="ddp_find_unused_parameters_true",
-    #         )
+            trainer = L.Trainer(
+                num_sanity_val_steps=0,
+                limit_train_batches=1,
+                limit_val_batches=1,
+                max_epochs=1,
+                devices=[0],
+                accelerator="gpu",
+                strategy="ddp_find_unused_parameters_true",
+            )
 
-    #         trainer.fit(model, datamodule=data_module)
+            trainer.fit(model, datamodule=data_module)
 
-    # def test_evaluate_arboel_biencoder(self):
-    #     """
-    #     TestCase - 2: Test the evaluate part of Biencoder
-    #     """
+    def test_evaluate_arboel_biencoder(self):
+        """
+        TestCase - 2: Test the evaluate part of Biencoder
+        """
 
-    #     # Load configuration file
-    #     with open("config_arboel.json", "r") as f:
-    #         config = ujson.load(f)
+        # Load configuration file
+        with open("config_arboel.json", "r") as f:
+            config = ujson.load(f)
 
-    #     test_cases = config["test_cases_eval_biencoder"]
+        test_cases = config["test_cases_eval_biencoder"]
 
-    #     parser = BlinkParser(add_model_args=True)
-    #     parser.add_eval_args()
+        parser = BlinkParser(add_model_args=True)
+        parser.add_eval_args()
 
-    #     for case in test_cases:
+        for case in test_cases:
 
-    #         test_args = [
-    #             "--data_path",
-    #             case["data_path"],
-    #             "--output_path",
-    #             case["output_path"],
-    #             "--dataset",
-    #             case["dataset"],
-    #             "--ontology",
-    #             case["ontology"],
-    #             "--load_function",
-    #             case["load_function"],
-    #             "--biencoder_checkpoint",
-    #             case["biencoder_checkpoint"],
-    #             "--model_name_or_path",
-    #             "dmis-lab/biobert-base-cased-v1.2",
-    #         ]
+            test_args = [
+                "--data_path",
+                case["data_path"],
+                "--output_path",
+                case["output_path"],
+                "--dataset",
+                case["dataset"],
+                "--ontology",
+                case["ontology"],
+                "--load_function",
+                case["load_function"],
+                "--biencoder_checkpoint",
+                case["biencoder_checkpoint"],
+                "--model_name_or_path",
+                "dmis-lab/biobert-base-cased-v1.2",
+            ]
 
-    #         args = parser.parse_args(test_args)
-    #         args_dict = args.__dict__
-    #         args_dict["ontology_dict"] = case["ontology_dict"]
-    #         # Check if the function name provided is an actual function of BiomedicalOntology
-    #         if hasattr(BiomedicalOntology, args_dict["load_function"]):
-    #             load_func = getattr(BiomedicalOntology, args_dict["load_function"])
+            args = parser.parse_args(test_args)
+            args_dict = args.__dict__
+            args_dict["ontology_dict"] = case["ontology_dict"]
+            # Check if the function name provided is an actual function of BiomedicalOntology
+            if hasattr(BiomedicalOntology, args_dict["load_function"]):
+                load_func = getattr(BiomedicalOntology, args_dict["load_function"])
 
-    #             if args_dict["ontology_dict"]:
-    #                 ontology_object = load_func(**args_dict["ontology_dict"])
-    #                 print(f"Ontology loaded successfully. Name: {ontology_object.name}")
-    #             else:
-    #                 raise ValueError("No ontology data provided.")
-    #         else:
-    #             raise ValueError(
-    #                 f"Error: {args_dict['load_function']} is not a valid function for BiomedicalOntology."
-    #             )
+                if args_dict["ontology_dict"]:
+                    ontology_object = load_func(**args_dict["ontology_dict"])
+                    print(f"Ontology loaded successfully. Name: {ontology_object.name}")
+                else:
+                    raise ValueError("No ontology data provided.")
+            else:
+                raise ValueError(
+                    f"Error: {args_dict['load_function']} is not a valid function for BiomedicalOntology."
+                )
 
-    #         with open(
-    #             os.path.join(
-    #                 args_dict["data_path"], f"{args_dict['ontology']}_object.pickle"
-    #             ),
-    #             "wb",
-    #         ) as f:
-    #             pickle.dump(ontology_object, f)
+            with open(
+                os.path.join(
+                    args_dict["data_path"], f"{args_dict['ontology']}_object.pickle"
+                ),
+                "wb",
+            ) as f:
+                pickle.dump(ontology_object, f)
 
-    #         data_module = ArboelDataModule(params=args_dict)
+            data_module = ArboelDataModule(params=args_dict)
 
-    #         model = LitArboel.load_from_checkpoint(
-    #             params=args_dict, checkpoint_path=args_dict["biencoder_checkpoint"]
-    #         )
+            model = LitArboel.load_from_checkpoint(
+                params=args_dict, checkpoint_path=args_dict["biencoder_checkpoint"]
+            )
 
-    #         trainer = L.Trainer(
-    #             limit_test_batches=1,
-    #             devices=[0],
-    #             accelerator="gpu",
-    #             strategy="ddp_find_unused_parameters_true",
-    #         )
+            trainer = L.Trainer(
+                limit_test_batches=1,
+                devices=[0],
+                accelerator="gpu",
+                strategy="ddp_find_unused_parameters_true",
+            )
 
-    #         trainer.test(model=model, datamodule=data_module)
+            trainer.test(model=model, datamodule=data_module)
 
     def test_train_arboel_crossencoder(self):
         """
