3469a313118e551df8ed2a3bc8dd25c2435ed607
Ye_Christophe_cye73 <cye73@MITCH-LIN.AD.GATECH.EDU>
2024-06-04 21:14:59 -0400

    Creates the Model_Wrapper

--- bioel/bioel/model.py
+++ bioel/bioel/model.py
@@ -1,25 +1,112 @@
-import subprocess
+from bioel.models.arboel.biencoder.model.BiEncoderLightningModule import LitArboel
+from bioel.models.arboel.crossencoder.model.CrossEncoderLightningModule import (
+    LitCrossEncoder,
+)
+from bioel.models.arboel.biencoder.model.common.params import BlinkParser
+import json
+import importlib
+import argparse
+import lightning as L
+import os
+
 
 class Model_Wrapper:
-    name: str  # name of the model
-    model: object  # model object
-    train_script_path: str  # path to the training script
-    evaluate_script_path: str  # path to the evaluation script
-    params: dict  # model arguments for training and inferencing
+    def __init__(self, model, name, train_script_path, evaluate_script_path, params):
+        self.name = name  # name of the model
+        self.model = model  # model object
+        self.train_script_path = train_script_path  # path to the training script
+        self.evaluate_script_path = (
+            evaluate_script_path  # path to the evaluation script
+        )
+        self.params = params  # model arguments for training and inferencing
 
     @classmethod
-    def load_arboel_biencoder(cls, name, params):
-        model = model
-        train_script_path = f"bioel/models/{name}/biencoder/train.py"
-        evaluate_script_path = f"bioel/models/{name}/biencoder/evaluate.py"
-        return cls(model, name, train_script_path, evaluate_script_path, params)
+    def load_arboel_biencoder(cls, name, params_file, checkpoint_path=None):
+        # Check if params_file is a path or a dictionary
+        if isinstance(params_file, str) and os.path.isfile(params_file):
+            with open(params_file, "r") as f:
+                json_params = json.load(f)
+        elif isinstance(params_file, dict):
+            json_params = params_file
+        else:
+            raise TypeError("params_file must be a valid file path or a dictionary")
+
+        parser = BlinkParser(add_model_args=True)
+        parser.add_training_args()
+        parser.add_eval_args()
+
+        # Set default values and remove required flag for params in JSON
+        for action in parser._actions:
+            if action.dest in json_params:
+                parser.set_defaults(**{action.dest: json_params[action.dest]})
+                action.required = False
+
+        # Parse arguments
+        args = parser.parse_args()
+        params = vars(args)
+        params.update(json_params)
+
+        if checkpoint_path:
+            model = LitArboel.load_from_checkpoint(
+                checkpoint_path=checkpoint_path, params=params
+            )
+        else:
+            model = LitArboel(params=params)
+
+        train_script_path = f"bioel/models/arboel/biencoder/train_biencoder.py"
+        evaluate_script_path = f"bioel/models/arboel/biencoder/evaluate_biencoder.py"
+        return cls(
+            model,
+            name,
+            train_script_path,
+            evaluate_script_path,
+            params,
+        )
 
     @classmethod
-    def load_arboel_crossencoder(cls, name, params):
-        model = model
-        train_script_path = f"bioel/models/{name}/crossencoder/train.py"
-        evaluate_script_path = f"bioel/models/{name}/crossencoder/evaluate.py"
-        return cls(model, name, train_script_path, evaluate_script_path, params)
+    def load_arboel_crossencoder(cls, name, params_file, checkpoint_path=None):
+        # Check if params_file is a path or a dictionary
+        if isinstance(params_file, str) and os.path.isfile(params_file):
+            with open(params_file, "r") as f:
+                json_params = json.load(f)
+        elif isinstance(params_file, dict):
+            json_params = params_file
+        else:
+            raise TypeError("params_file must be a valid file path or a dictionary")
+
+        parser = BlinkParser(add_model_args=True)
+        parser.add_training_args()
+        parser.add_eval_args()
+
+        # Set default values and remove required flag for params in JSON
+        for action in parser._actions:
+            if action.dest in json_params:
+                parser.set_defaults(**{action.dest: json_params[action.dest]})
+                action.required = False
+
+        # Parse arguments
+        args = parser.parse_args()
+        params = vars(args)
+        params.update(json_params)
+
+        if checkpoint_path:
+            model = LitCrossEncoder.load_from_checkpoint(
+                checkpoint_path=checkpoint_path, params=params
+            )
+        else:
+            model = LitCrossEncoder(params=params)
+
+        train_script_path = f"bioel/models/arboel/crossencoder/train_crossencoder.py"
+        evaluate_script_path = (
+            f"bioel/models/arboel/crossencoder/evaluate_crossencoder.py"
+        )
+        return cls(
+            model,
+            name,
+            train_script_path,
+            evaluate_script_path,
+            params,
+        )
 
     @classmethod
     def load_sapbert(cls, name, params):
@@ -42,9 +129,27 @@ class Model_Wrapper:
         pass
 
     def training(self):
-        # Call the specified train.py script using subprocess, passing the dict as an argument
-        subprocess.run(["python", self.train_script_path, "--params", self.params])
+        module_name = self.train_script_path.replace("/", ".").rsplit(".", 1)[0]
+        train_module = importlib.import_module(module_name)
+        train_module.train_model(params=self.params, model=self.model)
 
     def inference(self):
-        # Call the specified evalaute.py script using subprocess, passing the dict as an argument
-        subprocess.run(["python", self.evaluate_script_path, "--params", self.params])
+        module_name = self.evaluate_script_path.replace("/", ".").rsplit(".", 1)[0]
+        evaluate_module = importlib.import_module(module_name)
+        evaluate_module.evaluate_model(self.params, self.model)
+
+
+if __name__ == "__main__":
+    arboel_biencoder = Model_Wrapper.load_arboel_biencoder(
+        name="arboel_biencoder",
+        params_file="/home2/cye73/data_test2/arboel/ncbi_disease/params_biencoder.json",
+    )
+    arboel_biencoder.training()
+    arboel_biencoder.inference()
+
+    arboel_cross = Model_Wrapper.load_arboel_crossencoder(
+        name="arboel_crossencoder",
+        params_file="/home2/cye73/data_test2/arboel/ncbi_disease/params_crossencoder.json",
+    )
+    arboel_cross.training()
+    arboel_cross.inference()
--- bioel/bioel/models/arboel/crossencoder/train_crossencoder.py
+++ bioel/bioel/models/arboel/crossencoder/train_crossencoder.py
@@ -15,38 +15,36 @@ import os
 import torch
 
 
-def main(args):
-    print("Current seed:", args["seed"])
+def train_model(params, model):
+    print("Current seed:", params["seed"])
 
-    prepare_data(args)
+    prepare_data(params)
 
-    data_module = CrossEncoderDataModule(params=args)
-
-    model = LitCrossEncoder(params=args)
+    data_module = CrossEncoderDataModule(params=params)
 
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     model_checkpoint = ModelCheckpoint(
         monitor="Accuracy",  # Metric to monitor
-        dirpath=args["output_path"],  # Directory to save the model
+        dirpath=params["output_path"],  # Directory to save the model
         filename=f"crossencoder_{current_time}-{{epoch}}-{{Accuracy:.2f}}",  # Saves the model with epoch and val_loss in the filename
         save_top_k=1,  # Number of best models to save; -1 means save all of them
         mode="max",  # 'max' means the highest max_acc will be considered as the best model
         verbose=True,  # Logs a message whenever a model checkpoint is saved
     )
 
-    wandb_logger = WandbLogger(project=args["experiment"])
+    wandb_logger = WandbLogger(project=params["experiment"])
 
     trainer = L.Trainer(
         limit_val_batches=20,
         # num_sanity_val_steps=0,
         # fast_dev_run=True,
-        max_epochs=args["num_train_epochs"],
-        devices=args["devices"],
-        accelerator="gpu" if args["devices"] else "cpu",
+        max_epochs=params["num_train_epochs"],
+        devices=params["devices"],
+        accelerator="gpu" if params["devices"] else "cpu",
         strategy="ddp_find_unused_parameters_true",
         enable_progress_bar=True,
         callbacks=[model_checkpoint],
-        accumulate_grad_batches=args["gradient_accumulation_steps"],
+        accumulate_grad_batches=params["gradient_accumulation_steps"],
         precision="16-mixed",
         check_val_every_n_epoch=1,
         logger=wandb_logger,
@@ -55,9 +53,49 @@ def main(args):
     trainer.fit(model, datamodule=data_module)
 
 
-if __name__ == "__main__":
-    parser = BlinkParser(add_model_args=True)
-    parser.add_training_args()
-    args = parser.parse_args()
-    print(args)
-    main(args.__dict__)
+# def main(args):
+#     print("Current seed:", args["seed"])
+
+#     prepare_data(args)
+
+#     data_module = CrossEncoderDataModule(params=args)
+
+#     model = LitCrossEncoder(params=args)
+
+#     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     model_checkpoint = ModelCheckpoint(
+#         monitor="Accuracy",  # Metric to monitor
+#         dirpath=args["output_path"],  # Directory to save the model
+#         filename=f"crossencoder_{current_time}-{{epoch}}-{{Accuracy:.2f}}",  # Saves the model with epoch and val_loss in the filename
+#         save_top_k=1,  # Number of best models to save; -1 means save all of them
+#         mode="max",  # 'max' means the highest max_acc will be considered as the best model
+#         verbose=True,  # Logs a message whenever a model checkpoint is saved
+#     )
+
+#     wandb_logger = WandbLogger(project=args["experiment"])
+
+#     trainer = L.Trainer(
+#         limit_val_batches=20,
+#         # num_sanity_val_steps=0,
+#         # fast_dev_run=True,
+#         max_epochs=args["num_train_epochs"],
+#         devices=args["devices"],
+#         accelerator="gpu" if args["devices"] else "cpu",
+#         strategy="ddp_find_unused_parameters_true",
+#         enable_progress_bar=True,
+#         callbacks=[model_checkpoint],
+#         accumulate_grad_batches=args["gradient_accumulation_steps"],
+#         precision="16-mixed",
+#         check_val_every_n_epoch=1,
+#         logger=wandb_logger,
+#     )
+
+#     trainer.fit(model, datamodule=data_module)
+
+
+# if __name__ == "__main__":
+#     parser = BlinkParser(add_model_args=True)
+#     parser.add_training_args()
+#     args = parser.parse_args()
+#     print(args)
+#     main(args.__dict__)
